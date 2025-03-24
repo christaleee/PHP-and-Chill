2b38ffee6835f4607c5a783002431a56b396c0f8
mikshujeong <ellaablaza816@gmail.com>
2025-03-25 00:36:45 +0800

    Update player_details_page.php

--- Crimson Legacy/player_details_page.php
+++ Crimson Legacy/player_details_page.php
@@ -110,11 +110,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update_id']) && isset(
         </div>
         <?php endforeach; ?>
         </div>
-        <?php if (count($_SESSION['form_data']) < 4): ?> 
-            <div class="addPlayer"> 
-                <a class="addPlayerButton" style="--clr:#c51a1a" href="player_form.php"><i></i><span>ADD NEW PLAYER</span></a>
-            </div>
-        <?php endif; ?>
+        <div class="addPlayer">
+            <a class="addPlayerButton"style="--clr:#c51a1a" href="welcome_page.php"><i></i><span>ADD NEW PLAYER</span></a>
+        </div>
 
     </div>
     </div>
